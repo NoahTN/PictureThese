e6304bf7acfdeaf81c87faf90599641c8738b06d
NoahTolentinoNguyen <ntolentino-nguyen@csumb.edu>
2019-04-07 15:48:36 -0700

    Add initial website structure

---
+++ js/upload.js
@@ -0,0 +1,24 @@
+const defaultUploadBtn = document.getElementById("default-upload-btn");
+const customUploadBtn = document.getElementById("custom-upload-btn");
+const customUploadText = document.getElementById("custom-upload-text");
+const imageDiv = document.getElementById("image-div");
+const image = document.getElementById("uploaded-img");
+const itemsDiv = document.getElementById("items-div");
+
+customUploadBtn.addEventListener("click", function() {
+    defaultUploadBtn.click();
+});
+
+defaultUploadBtn.addEventListener("change", function() {
+    if(defaultUploadBtn.value) {
+        imageDiv.style.display = "block";
+        image.src = URL.createObjectURL(defaultUploadBtn.files[0]);
+        image.style.height = "500px";
+        console.log(imageDiv.style.height);
+        customUploadText.innerHTML = defaultUploadBtn.value.split("\\").pop();
+    }
+    else {
+        customUploadText.innerHTML = "No file chosen";
+    }
+});
+
