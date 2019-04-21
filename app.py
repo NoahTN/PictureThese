533ecf33daa7b41ed1f90cf1a8cb63a7bf998140
NoahTolentinoNguyen <ntolentino-nguyen@csumb.edu>
2019-04-20 21:38:16 -0700

    Add credential functionality for heroku

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 
 import io
 import os
+import json
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
+from google.oauth2 import service_account
 from google.cloud import vision
 from google.cloud.vision import types
 
 app = Flask(__name__)
-client = vision.ImageAnnotatorClient()
+
+# Generate credentials
+credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+service_account_info = json.loads(credentials_raw)
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
+
+client = vision.ImageAnnotatorClient(credentials=credentials)
 
 @app.route('/')
 def index():
