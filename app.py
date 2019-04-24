5a7fbab59b942fb84767d37fc5cb4d623290cf11
NoahTolentinoNguyen <ntolentino-nguyen@csumb.edu>
2019-04-24 12:32:39 -0700

    Refactor image uploading

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import io
 import os
 import json
@@ -10,13 +9,12 @@ from google.cloud.vision import types
 
 app = Flask(__name__)
 
-# Generate credentials 
+# Instantiates a vision client
 # May have to comment out to get to work locally
 credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 service_account_info = json.loads(credentials_raw)
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
-
-client = vision.ImageAnnotatorClient(credentials=credentials)
+client = vision.ImageAnnotatorClient()
 
 @app.route('/')
 def index():
@@ -24,39 +22,23 @@ def index():
 
 @app.route('/image', methods = ['POST'])
 def visionAPI ():
+
 	if request.method == 'POST':
 		f = request.files['file']
-		sfname = 'static/images/'+str(secure_filename(f.filename))
-		f.save(sfname)
+		content = f.read()
 
-	# The name of the image file to annotate
-	file_name = os.path.join(
-	    os.path.dirname(__file__), 'static/images/'+ str(f.filename))
-
-	# Loads the image into memory
-	with io.open(file_name, 'rb') as image_file:
-	    content = image_file.read()
-	
+	# Reads in image to object
 	image = types.Image(content=content)
 
-	#These 2 lines allow to look at image from url
-	#image = types.Image()
-	#image.source.image_uri = image_link
-
-	# Performs label detection on the image file
+	# Performs label detection on the image object
 	response = client.label_detection(image=image)
 	labels = response.label_annotations
-	'''
-	print('Labels:')
-	for label in labels:
-	    print(label.description)
-	'''
+
 	labelStr = ""
 	for label in labels:
 		labelStr += label.description
 
 	#return render_template("index.html")
-	
 	return labelStr
 
 if __name__=='__main__':
