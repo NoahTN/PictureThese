f84cfec92eabc80447f2e8775e0cdf7efba2b2fc
jabernall24 <jabernall24@gmail.com>
2019-04-25 13:02:39 -0700

    User is able to select from all available languagues

--- app.py
+++ app.py
@@ -6,22 +6,39 @@ from werkzeug import secure_filename
 from google.oauth2 import service_account
 from google.cloud import vision
 from google.cloud.vision import types
+from google.cloud import translate
+
+# Pycharm local env
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 
 app = Flask(__name__)
 
 # Instantiates a vision client
 # May have to comment out to get to work locally
-credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-service_account_info = json.loads(credentials_raw)
-credentials = service_account.Credentials.from_service_account_info(service_account_info)
-client = vision.ImageAnnotatorClient(credentials=credentials)
+# credentials_raw = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+# credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+# service_account_info = json.loads(credentials_raw)
+# credentials = service_account.Credentials.from_service_account_info(service_account_info)
+client = vision.ImageAnnotatorClient()#credentials=credentials)
+
+# Translate client
+translate_client = translate.Client()
+
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+	languages = translate_client.get_languages()
 
-@app.route('/image', methods = ['POST'])
-def visionAPI ():
+	return render_template("index.html", languages=languages)
+
+
+@app.route('/image', methods=['POST'])
+def vision_api():
 
 	if request.method == 'POST':
 		f = request.files['file']
@@ -34,12 +51,19 @@ def visionAPI ():
 	response = client.label_detection(image=image)
 	labels = response.label_annotations
 
-	labelStr = ""
+	label_str = ""
+	print(translate_api(labels[0].description, "fr"))
 	for label in labels:
-		labelStr += label.description
+		label_str += label.description
 
 	#return render_template("index.html")
-	return labelStr
+	return label_str
 
-if __name__=='__main__':
-    app.run()
+
+def translate_api(word, translate_to):
+	result = translate_client.translate(word, target_language=translate_to, source_language="en")
+	return result
+
+
+if __name__ == '__main__':
+	app.run()
