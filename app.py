c55437a476d0da437b4513223fc37a29189e1210
NoahTolentinoNguyen <42818569+NoahTolentinoNguyen@users.noreply.github.com>
2019-04-25 16:03:38 -0700

    Fix for webserver

--- app.py
+++ app.py
@@ -7,39 +7,35 @@ from google.oauth2 import service_account
 from google.cloud import vision
 from google.cloud.vision import types
 from google.cloud import translate
-
-# Pycharm local env
+'''
+Pycharm local env
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
+'''
 
 app = Flask(__name__)
 
 # Instantiates a vision client
 # May have to comment out to get to work locally
-# credentials_raw = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-# credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-# service_account_info = json.loads(credentials_raw)
-# credentials = service_account.Credentials.from_service_account_info(service_account_info)
-client = vision.ImageAnnotatorClient()#credentials=credentials)
+credentials_raw = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+credentials_raw = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+service_account_info = json.loads(credentials_raw)
+credentials = service_account.Credentials.from_service_account_info(service_account_info)
+client = vision.ImageAnnotatorClient(credentials=credentials)
 
 # Translate client
 translate_client = translate.Client()
 
-
 @app.route('/')
 def index():
 	languages = translate_client.get_languages()
-
 	return render_template("index.html", languages=languages)
-
 
 @app.route('/image', methods=['POST'])
 def vision_api():
-
 	if request.method == 'POST':
 		f = request.files['file']
 		content = f.read()
@@ -53,18 +49,15 @@ def vision_api():
 
 	# TODO: return proper json object with keys and values
 	label_str = ""
-
 	for label in labels:
 		label_str += label.description
 
 	#return render_template("index.html")
 	return label_str
 
-
 def translate_api(word, translate_to):
 	result = translate_client.translate(word, target_language=translate_to, source_language="en")
 	return result
-
 
 if __name__ == '__main__':
 	app.run()
